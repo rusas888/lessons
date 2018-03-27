988f74c64d8e6ebdd7f6571511e1c3fffad7b4bb
Rus As <rusas@Russ-MacBook-Air.local>
2018-03-27 15:17:24 +0600

    a

--- configs.js
+++ configs.js
@@ -2,6 +2,6 @@ module.exports = {
 	database: {
 		host: '192.168.4.196',
 		port: 27017,
-		url: 'mongodb://192.168.4.196:27017/lesson11mongo'
+		url: 'mongodb://admin:AKLbif64267@192.168.4.203:27017/lessons'
 	}
 }
