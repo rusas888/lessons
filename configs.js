ff7c235a848a80ac6eda2287c8eb40bcab50f76c
Rus As <rusas@Russ-MacBook-Air.local>
2018-03-27 18:57:11 +0600

    a

--- configs.js
+++ configs.js
@@ -1,7 +1,7 @@
 module.exports = {
 	database: {
-		host: '192.168.4.196',
+		host: '192.168.4.203',
 		port: 27017,
-		url: 'mongodb://admin:AKLbif64267@192.168.4.203:27017/lessons'
+		url: 'mongodb://192.168.4.203:27017/lessons'
 	}
 }
--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const Mongoose = require('mongoose')
 const Config = require('./configs')
 
-Mongoose.connect(Config.database.url);
+Mongoose.connect(Config.database.url, {
+	"user": "admin",
+    "pass": "AKLbif64267@"
+});
 const db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
